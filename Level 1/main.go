1709ff2afba0e078a752107ad3591c3ba71aed3a
Hanif <hanif.kharismadini.04@gmail.com>
2024-01-17 07:46:54 +0700

    level 1: submit task and code

--- Level 1/main.go
+++ Level 1/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"rsc.io/quote/v4"
 )
 
 func main() {
-	fmt.Sprintln(quote.Go())
-	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+	fmt.Println("Hello, world!")
+
+	const name, age = "Hanif", 30
+	s := fmt.Sprintln(name, "is", age, "years old.")
+	fmt.Print(s)
+
+	e := fmt.Errorf("%s", "data not found").Error()
+	fmt.Print(e)
+
+	err := errors.New("failed get data")
+	fmt.Print(err)
 }
